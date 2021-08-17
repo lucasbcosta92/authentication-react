9f850aa30d897910cf8b476995fee8ba8ded2c91
lucas.costa <lucas.costa@acct.global>
2021-08-17 14:30:25 -0300

    feat: added auth token in cookie

--- contexts/AuthContexts.tsx
+++ contexts/AuthContexts.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
-import { setCookie } from "nookies";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies } from "nookies";
 import { api } from "../services/api";
 
 type User = {
@@ -29,6 +29,22 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies(); // get cookies
+
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({
+          email,
+          permissions,
+          roles,
+        });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post("sessions", {
@@ -42,13 +58,18 @@ export function AuthProvider({ children }: AuthProviderProps) {
         maxAge: 60 * 60 * 24 * 30, // 30 dias - tempo de expiração
         path: "/", // Caminhos que terão acesso ao cookie - Global
       });
-      setCookie(undefined, "nextauth.refreshToken", refreshToken);
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        path: "/",
+      });
 
       setUser({
         email,
         permissions,
         roles,
       });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
     } catch (error) {
       console.log(error);
     }
